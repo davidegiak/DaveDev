f20fb6e6652649e610fbe5f6ddb8fda0cfc25d32
Davide Di Giacomo <159280067+davidegiak@users.noreply.github.com>
2024-12-04 18:52:53 +0100

    base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/deploying-vite-project-DaveDev',
   plugins: [vue()],
+  base: 'https://davidegiak.github.io/DaveDev/'
 })
