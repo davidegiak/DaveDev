ee1e6ae5dd79c7cadd488cd248eec64e5d621f26
Davide Di Giacomo <159280067+davidegiak@users.noreply.github.com>
2024-12-10 17:23:37 +0100

    prova 7

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './style.css'
 import { router } from './router.js';
-import App from './App.vue'
+import App from './viewa/Main.vue'
 
 createApp(App).use(router).mount('#app')
