d12367b52e69fbb5644ab6c649e5f1aba30e63b9
Davide Di Giacomo <159280067+davidegiak@users.noreply.github.com>
2024-12-10 17:27:02 +0100

    back

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './style.css'
 import { router } from './router.js';
-import App from './views/Main.vue'
+import App from './App.vue'
 
 createApp(App).use(router).mount('#app')
