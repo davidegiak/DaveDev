579a076990fc0a40b1b5de7c1e514c1f1bae1fd7
Davide Di Giacomo <159280067+davidegiak@users.noreply.github.com>
2024-12-10 17:37:57 +0100

    prova png 3

--- src/picies/Jumbo.vue
+++ src/picies/Jumbo.vue
@@ -7,7 +7,7 @@ export default {
     data() {
         return {
             store,
-            jumboImg: 'public/img/foto_curriculum-no-back.png',
+            jumboImg: '/public/img/foto_curriculum-no-back.png',
         }
     }
 }
@@ -22,7 +22,7 @@ export default {
             </h2>
         </div>
         <div class="col-6 col-sm-6 col-md-5 col-lg-4">
-            <img class="jumboImg" src="/public/img/foto_curriculum-no-back.png" style="">
+            <img class="jumboImg" :src="jumboImg" style="">
 
         </div>
     </div>
